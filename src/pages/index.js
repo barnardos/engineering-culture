4aaa9a1b16df75a13d745b5746953e71a70682f4
Toby Osbourn <toby.osbourn@gmail.com>
2019-03-07 15:34:25 +0000

    Add boilerplate for the culture
    First stab and getting something on paper. Expect there to be
    many changes very quickly.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {
   Submit,
+  BulletedList,
+  ListItem,
   Main,
   Hero,
   Title,
@@ -22,9 +24,50 @@ export default () => (
       <Heading>{`Our culture, our values`}</Heading>
       <Paragraph>{`This document outlines the outlines and culture of the #FutureBarnardos engineering team.`}</Paragraph>
       <Paragraph>
-        {`It is a living document. `}
-        <Link href={`/`}>{`Changes are encouraged`}</Link>
+        {`This is a living document. `}
+        <Link
+          href={`https://github.com/barnardos/engineering-culture`}
+        >{`Changes are encouraged`}</Link>
+        {`.`}
       </Paragraph>
+      <Paragraph>{`This document is high level by design, more practical guidance will be found in our Digital Playbook`}</Paragraph>
+      <Heading>{`Open`}</Heading>
+      <Paragraph>{`We default to openness:`}</Paragraph>
+      <BulletedList>
+        <ListItem>{`code in the open`}</ListItem>
+        <ListItem>{`prefer open source code to closed source`}</ListItem>
+        <ListItem>{`contribute back`}</ListItem>
+        <ListItem>{`communicate openly`}</ListItem>
+        <ListItem>{`build transparency into our systems`}</ListItem>
+      </BulletedList>
+      <Heading>{`Honest`}</Heading>
+      <Paragraph>{`We speak our truth:`}</Paragraph>
+      <BulletedList>
+        <ListItem>{`face uncomfortable problems`}</ListItem>
+        <ListItem>{`give clear feedback`}</ListItem>
+        <ListItem>{`own up to mistakes`}</ListItem>
+      </BulletedList>
+      <Heading>{`Clear`}</Heading>
+      <Paragraph>{`We work with clarity:`}</Paragraph>
+      <BulletedList>
+        <ListItem>{`share with clear intent`}</ListItem>
+        <ListItem>{`write clear code, not smart code`}</ListItem>
+        <ListItem>{`code and talk in human first`}</ListItem>
+      </BulletedList>
+      <Heading>{`Calm`}</Heading>
+      <Paragraph>{`We instil calmness in the organisation:`}</Paragraph>
+      <BulletedList>
+        <ListItem>{`engineer away surprises`}</ListItem>
+        <ListItem>{`take time to consider the impact of our actions`}</ListItem>
+        <ListItem>{`think before we code`}</ListItem>
+      </BulletedList>
+      <Heading>{`Listen`}</Heading>
+      <Paragraph>{`We do not know best:`}</Paragraph>
+      <BulletedList>
+        <ListItem>{`listen once, then listen again`}</ListItem>
+        <ListItem>{`seek clarity before comitting`}</ListItem>
+        <ListItem>{`write code that can respond to feedback`}</ListItem>
+      </BulletedList>
     </Main>
   </Page>
 );
